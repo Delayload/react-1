ab501d15370b498a4555063640aa41d89b9e95b6
delayload <alexjaworski88@gmail.com>
2020-11-01 23:00:59 +0200

    used connect in post

--- src/App.js
+++ src/App.js
@@ -2,30 +2,24 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header.jsx';
 import Activity from './components/Activity/Activity';
-import { BrowserRouter, Route } from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import Profile from './components/Profile/Profile';
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-const App = (props) => {
+
+const App = () => {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header/>
-        <div className='container'>
-          <Route path='/profile' component={Profile}/>
-          <Route path='/activity' render={() => 
-            <Activity 
-              postsPage = {props.state.postsPage}
-              dispatch = {props.dispatch}
-            />
-          }/>
-          <Route path='/dialogs' render={() => 
-            <DialogsContainer
-              state = {props.state}
-              dispatch = {props.dispatch}/>
-          }/>
-        </div>
+    <div className="app-wrapper">
+      <Header/>
+      <div className='container'>
+        <Route path='/profile' component={Profile}/>
+        <Route path='/activity' render={() =>
+          <Activity/>
+        }/>
+        <Route path='/dialogs' render={() =>
+          <DialogsContainer/>
+        }/>
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
